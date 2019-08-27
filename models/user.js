42cc8aab0e3f320434058b944adffbb5165b693e
Davi Cardoso <20673538+daviccardoso@users.noreply.github.com>
2019-08-27 15:52:29 -0300

    Criação do modelo de usuários

---
+++ models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const userSchema = Schema(
+  {
+    'name': {
+      'first': {
+        type: String,
+        trim: true
+      },
+      'last': {
+        type: String,
+        trim: true
+      }
+    },
+    'email': {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true
+    },
+    'zipCode': {
+      type: Number,
+      min: [10000, 'Zip code too short'],
+      max: 99999
+    },
+    'password': {
+      type: String,
+      required: true
+    },
+    'courses': [{
+      type: Schema.Types.ObjectId,
+      ref: 'Course'
+    }],
+    'subscribedAccount': {
+      type: Schema.Types.ObjectId,
+      ref: 'Subscriber'
+    }
+  },
+  {
+    timestamp: true
+  }
+);
+
+userSchema.virtual('fullName')
+  .get(function() {
+    return `${this.name.first} ${this.name.last}`;
+  });
+
+module.exports = mongoose.model('User', userSchema);
