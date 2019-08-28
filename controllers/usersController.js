fe73d19f76fa5981c860975505ceab6fdc0c7f8c
Davi Cardoso <20673538+daviccardoso@users.noreply.github.com>
2019-08-28 12:36:07 -0300

    Criação de controller de usuários

---
+++ controllers/usersController.js
@@ -0,0 +1,19 @@
+const User = require('../models/user');
+
+function index(req, res, next) {
+  User.find({})
+    .then(users => {
+      res.locals.users = users;
+      next();
+    })
+    .catch(error => {
+      console.log(`Error fetching users: ${error.messafe}`);
+      next(error);
+    });
+}
+
+function indexView(req, res) {
+  res.render('users/index');
+}
+
+module.exports = { index, indexView };
